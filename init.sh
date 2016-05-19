e5f2436cf66e7ec323cac03b98c75bc0a7c8f7eb
ivan <yaivansemin@yandex.ru>
2016-05-19 20:51:54 +0300

    fix config

--- init.sh
+++ init.sh
@@ -7,7 +7,7 @@ sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart 
 
-sudo /etc/init.d/mysql restart
+#sudo /etc/init.d/mysql restart
 mysql -uroot -e "create database myproject;"
 mysql -uroot -e "CREATE USER 'enth'@'localhost' IDENTIFIED BY 'password';"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON * . * TO 'enth'@'localhost';"
