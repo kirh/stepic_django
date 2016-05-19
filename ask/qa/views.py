a65b363afae687e0e05a005a4aaa396e6a30e76f
ivan <yaivansemin@yandex.ru>
2016-05-19 23:04:50 +0300

    fix config

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse 
+from django.core.paginator import Paginator
 
 from django.shortcuts import render 
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
+
+def post_list_all(request):
+    page = request.GET.get('page', 1)
