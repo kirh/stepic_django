71c826f15169aa4def0e884a7a6f9ef81233f619
ivan <yaivansemin@yandex.ru>
2016-05-19 20:48:47 +0300

    fix config

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -57,8 +57,12 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myproject',
+        'USER': 'enth',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
