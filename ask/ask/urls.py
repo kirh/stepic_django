23fb083c470fca83ee206f06b0b01f2b905a61ea
ivan <cleverbotan@gmail.com>
2016-05-21 18:36:51 +0300

    fixed all

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -9,8 +9,8 @@ urlpatterns = patterns('',
 	url(r'^$', 'qa.views.post_list_all', name='home'),
 	url(r'^login/.*$', 'qa.views.test', name='login'),
 	url(r'^signup/.*$', 'qa.views.test', name='signup'),
-	url(r'^question/\d+/$', 'qa.views.test', name='question'),
+	url(r'^question/(\d+)/$', 'qa.views.question', name='question'),
 	url(r'^ask/.*$', 'qa.views.test', name='ask'),
-	url(r'^popular/.*$', 'qa.views.test', name='popular'),
+	url(r'^popular/.*$', 'qa.views.popular', name='popular'),
 	url(r'^new/.*$', 'qa.views.test', name='new')
 )
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -2,9 +2,10 @@
 
 from django.http import HttpResponse
 from django.core.paginator import Paginator
-from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import render, Http404
 
-from models import Question
+from models import Question, Answer
 
 
 def test(request, *args, **kwargs):
@@ -20,4 +21,26 @@ def post_list_all(request):
     return render(request, 'index.html', {
         'questions': page.object_list,
         'paginator': paginator, 'page': page,
+    })
+
+def popular(request):
+    questions = Question.objects.all().order_by('-rating')
+    page = request.GET.get('page', 1)
+    paginator = Paginator(questions, 10)
+    paginator.baseurl = '/popular/?page='
+    page = paginator.page(page) # Page
+    return render(request, 'popular.html', {
+        'questions': page.object_list,
+        'paginator': paginator, 'page': page,
+    })
+
+def question(request, question_id):
+    try:
+        q = Question.objects.get(id=question_id)
+    except ObjectDoesNotExist, err:
+        raise Http404()
+
+    answers = Answer.objects.filter(question=q)[:]
+    return render(request, 'question.html', {
+        'answers': answers
     })
